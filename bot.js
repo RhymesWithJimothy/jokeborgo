549363b457ef414b39b217d4d1f3c6e037d4a09c
RhymesWithJimothy <31680807+RhymesWithJimothy@users.noreply.github.com>
2018-04-26 22:57:45 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,9 +20,44 @@ if(message.content.toLowerCase().includes( ['bepsi'] ) || message.content.toLowe
    }
 
  
+  
+  
+  
+var randomgun =  [":boom::gun:",
+":boom::gun:",
+"<a:loading:436659448446976010>:gun:",
+"<a:rainbOOM:439130744296701952><:rainbow_sparkle_gun:421211924840316938>",
+"<:BANGO:439181687759306752><:gunbutjokers:439271523631759361>"]  
+
+var randomgunmessage =  ["Looks like you have been **shot**. ʜᴀHA *!*",
+"Looks like you have been **shot**. ʜᴀHA *!*",
+"The gun is... loading? What.",
+"**ʏᴏᴜ ʜᴀᴠᴇ ʙᴇᴇɴ ᴋɪʟʟᴇᴅ ʙʏ ᴛʜᴇ ᴘᴏᴡᴇʀ ᴏғ ɢ ᴀ ʏ** ʜᴀHA**H*A*** *!*",
+"**ʜᴀHAAAʜᴏᴏHEEAʜᴀH*A*ʜ**"] 
+
+var numberofgun = randomgun.length - 1;
+var countgun = Math.round(Math.random() * (numberofgun - 1) + 1)
+ 
+   if(message.content.toLowerCase().startsWith(prefix + "gun")){
+       
+    message.channel.sendMessage(":gun:")
+   
+    .then(msg => { 
+        
+        setTimeout(function () {
+            msg.edit(randomgun[countgun])
+            message.channel.sendMessage(randomgunmessage[countgun])
+          }, 1000);
+         } )
+}
+  
+  
+  
+  
+  
 
 if (message.content.startsWith(prefix + "help"))  {
-message.channel.sendMessage(":bat: :black_joker:  A  **B A T J O K E S**  Bot   |   Prefix: `j!`\nCurrent commands: `joker` , `stab` , `clap` , `house` |  Command example: `j!joker`");
+message.channel.sendMessage(":bat: :black_joker:  A  **B A T J O K E S**  Bot   |   Prefix: `j!`\nCurrent commands: `joker` , `stab` , `gun` , `clap` , `house` |  Command example: `j!joker`");
 }
 
 
